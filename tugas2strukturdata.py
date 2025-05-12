68c680e15b04c38e367bae3a3c84d0284f1d7e2d
aloss01 <ganbarre07@gmail.com>
2025-05-12 23:04:49 +0800

    Add files via upload

---
+++ tugas2strukturdata.py
@@ -0,0 +1,11 @@
+print ("muhammad nur alam 24241123")
+inputuser = int ( input ("masukan angka yang bernilai kurang dari 15 atau lebih dari 24 "))
+# ++++24...........
+# memeriksa angka kurang dari 15
+iskurangdari = ( inputuser <15) 
+
+# memeriksa angkah yang lebih dari 24
+islebihdari = ( inputuser >24)
+
+final = iskurangdari or islebihdari 
+print ("angka yang anda masukan:",final)
