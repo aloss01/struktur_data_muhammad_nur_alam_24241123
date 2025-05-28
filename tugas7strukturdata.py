58c7321b5a04d0c104258c1fdc76df32aed5ce49
aloss01 <ganbarre07@gmail.com>
2025-05-29 02:08:33 +0800

    Add files via upload

---
+++ tugas7strukturdata.py
@@ -0,0 +1,11 @@
+tempat_lahir = input (" masukan tempat lahir anda :")
+
+# hasil = len(tempat_lahir)
+
+# hasil = tempat_lahir.find("y")
+# hasil = tempat_lahir.find("a")
+# hasil = tempat_lahir.capitalize()
+# hasil = tempat_lahir.upper()
+# hasil = tempat_lahir.lower()
+# hasil = tempat_lahir.istigit()
+hasil = tempat_lahir.isalpha()
