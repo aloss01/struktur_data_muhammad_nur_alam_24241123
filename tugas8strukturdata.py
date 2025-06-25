71305a8cbf77b21157a06adbc6593aa564d2ed80
aloss01 <ganbarre07@gmail.com>
2025-06-25 13:51:15 +0800

    Add files via upload

---
+++ tugas8strukturdata.py
@@ -0,0 +1,11 @@
+# Variabel number
+number = "1234567890"
+
+# A. Menampilkan data terakhir
+print("Data terakhir:", number[-1])
+
+# B. Menampilkan data pertama
+print("Data pertama:", number[0])
+
+# C. Menampilkan data ke-3 sampai ke-6
+print ( " data ke-3 sampai ke-5")
